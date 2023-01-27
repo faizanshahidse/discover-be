01f79b40e280ac1d8f6f536450e0cf89b8f59b9d
Faizan Shahid <faizan.shahid@by433.com>
2023-01-27 12:29:06 +0500

    TE-564 | fix: params validation added

--- src/validators/payloads/stream.payload.js
+++ src/validators/payloads/stream.payload.js
@@ -1,5 +1,7 @@
 /** Third party dependencies */
-const joi = require('joi')
+const joi = require('joi');
+
+const { isValidTimeZone } = require('../../utils/common');
 
 
 
@@ -14,13 +16,26 @@ const errorMessages = {
  * @param {Object} data 
  * @param {string} [data.excludeId] Exclusion id for pagination - Id of last document
  * @param {numeber} data.limit - Pagination limit
+ * @param {numeber} data.from - Pagination from
+ * @param {numeber} data.top_favourite_teams
+ * @param {string} data.tz
  */
 const streamQuery = async (data) => {
+
+    /** parse params into numbers */
+    data.from = data.from && parseInt(data.from);
+    data.limit = data.limit && parseInt(data.limit);
+    data.top_favourite_teams = data.top_favourite_teams && parseInt(data.top_favourite_teams);
+
+
     const streamValidator = joi.object({
-        limit: joi.string().required(),
-        from: joi.string().required(),
-        tz: joi.string().required(),
-        top_favourite_teams: joi.string().required()
+        limit: joi.number().required(),
+        from: joi.number().required(),
+        tz: joi.string().required().custom((value, helper) => {
+            if(isValidTimeZone(value)) return true;
+            else return helper.message('invalid timezone param');
+        }),
+        top_favourite_teams: joi.number().required()
     });
 
     const { error, ...rest } = await streamValidator.validate(data);
